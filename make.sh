7e17876e18bcb0f15d7dfe0b94e5d143a735403d
Hendra Manudinata <manoedinata@gmail.com>
2026-08-29 16:05:52 +0700

    chore: set all bash script to executables

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ROS_DISTRO=humble
-colcon build --symlink-install --executor parallel --parallel $(nproc)
+colcon build --symlink-install --executor parallel --parallel $(nproc) --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
